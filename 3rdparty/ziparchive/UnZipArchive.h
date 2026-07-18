d29dd9054c4c302409a06b207ce540532b373252
Andrew Udvare <audvare@gmail.com>
2026-07-18 01:03:56 -0400

    3rdparty/ziparchive: add modelines
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- 3rdparty/ziparchive/UnZipArchive.h
+++ 3rdparty/ziparchive/UnZipArchive.h
@@ -51,3 +51,7 @@
 - (NSData *)getData:(NSString *)entryName;
 - (void)closeFile;
 @end
+
+// kate: hl Objective-C; replace-tabs on; indent-width 4; tab-width 4;
+// vim: set ft=objc sw=4 ts=4 et :
+// code: language=Objective-C insertSpaces=true tabSize=4
