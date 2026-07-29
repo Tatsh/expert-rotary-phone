03f0aacaddad8605dea6978cafb3a00f6c2779f2
Andrew Udvare <audvare@gmail.com>
2026-07-29 02:13:08 +0000

    treasuremap: mark the header as Objective-C++ in the modelines
    TreasureMap.h imports <Foundation/Foundation.h>, so it is Objective-C++
    rather than plain C++. Switch the Kate, vim, and VSCode modelines to the
    Objective-C++ set, spelled the way the other Objective-C++ headers in
    the tree spell it.
    
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- Project/Game/Data/TreasureMap/TreasureMap.h
+++ Project/Game/Data/TreasureMap/TreasureMap.h
@@ -226,6 +226,6 @@ const char *getCharacterAssetName(int characterId, int slotIndex);
 // fidelity).
 int getTreasureMapValue_fb54(int unused, int mapId);
 
-// kate: hl C++; replace-tabs on; indent-width 4; tab-width 4;
-// vim: set ft=cpp sw=4 ts=4 et :
-// code: language=cpp insertSpaces=true tabSize=4
+// code: language=Objective-C++ insertSpaces=true tabSize=4
+// kate: hl Objective-C++; replace-tabs on; indent-width 4; tab-width 4;
+// vim: set ft=objcpp sw=4 ts=4 et :
